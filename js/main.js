7eb94af80b30c25318d76deef1dcc3c77272ac5e
Stan <lepikov.free@yandex.ru>
2015-02-09 21:29:53 +0300

    ae

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 (function($){
 
 	$(function() {
+        $('.callback').on('click', function() {
+            $('.popup').show()
+            $('.popup-substrate').height($(document).height())
+        });
 
+        $('.popup-substrate, .popup .close').on('click', function() {
+            $('.popup').hide()
+        });
     });
 
 })(jQuery,undefined)
